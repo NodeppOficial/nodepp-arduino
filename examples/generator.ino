b293c91dbdcca6049c5bf00a5f5ec0b67f5729be
EDBCREPO <109326461+EDBCREPO@users.noreply.github.com>
2023-11-29 00:11:13 -0400

    Update generator.ino

--- examples/generator.ino
+++ examples/generator.ino
@@ -2,32 +2,31 @@
 
 using namespace nodepp;
 
+using namespace nodepp;
+
 _Generator( process_1 ) {
     int counter = 100;
-    _Emit(int)
-
-    while( counter-->0 ){
-        console::done(" :>",counter);
-        _Return( counter );
-    }
-
-    _Stop
+    _Emit(){ _Start
+        while( counter-->0 ){
+            console::done(" :>",counter);
+            _Next;
+        }
+    _Stop }
 };
 
 _Generator( process_2 ) {
     int counter = 100;
-    _Emit(int)
-
-    while( counter-->0 ){
-        console::error(":>",counter);
-        _Return( counter );
-    }
-
-    _Stop
+    _Emit(){ _Start
+        while( counter-->0 ){
+            console::done(" :>",counter);
+            _Next;
+        }
+    _Stop }
 };
 
 void _Ready() {
     console::start(9600);
     process_1 A; process_2 B;
-    while( A()>0 && B()>0 ){}
+    process::loop::add(A);
+    process::loop::add(B);
 }
