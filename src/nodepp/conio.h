a156bba30f7d284e2a07071af8b724121f1fb3a7
edbc <becerracenmanueld@gmail.com>
2024-02-02 14:07:55 -0400

    update

--- src/nodepp/conio.h
+++ src/nodepp/conio.h
@@ -3,75 +3,79 @@
 
 /*────────────────────────────────────────────────────────────────────────────*/
 
-#define C_BLACK   0
-#define C_WHITE   1
-#define C_GREEN   2
-#define C_RED     3
-#define C_BLUE    4
-#define C_CYAN    5
-#define C_YELLOW  6
-#define C_MAGENTA 7
-#define C_BOLD    10
+#define C_BLACK   0x00
+#define C_WHITE   0x01
+#define C_GREEN   0x02
+#define C_RED     0x03
+#define C_BLUE    0x04
+#define C_CYAN    0x05
+#define C_YELLOW  0x06
+#define C_MAGENTA 0x07
+#define C_BOLD    0x10
 
 /*────────────────────────────────────────────────────────────────────────────*/
 
 namespace nodepp { namespace conio {
-
-    int background( int color ){ 
-        if( color >= 10 && color <= 17 ){ Serial.write("\033[1m"); color -= 10; }
-        switch( color ) {
-            case C_BLACK:   return Serial.write("\033[40m"); break;
-            case C_WHITE:   return Serial.write("\033[47m"); break;
-            case C_GREEN:   return Serial.write("\033[42m"); break;
-            case C_RED:     return Serial.write("\033[41m"); break;
-            case C_BLUE:    return Serial.write("\033[44m"); break;
-            case C_CYAN:    return Serial.write("\033[46m"); break;
-            case C_YELLOW:  return Serial.write("\033[43m"); break;
-            case C_MAGENTA: return Serial.write("\033[45m"); break;
-        }   return -1;
-    }
-
-    int foreground( int color ){
-        if( color >= 10 && color <= 17 ){ Serial.write("\033[1m"); color -= 10; }
-        switch( color ) {
-            case C_BLACK:   return Serial.write("\033[30m"); break;
-            case C_WHITE:   return Serial.write("\033[37m"); break;
-            case C_GREEN:   return Serial.write("\033[32m"); break;
-            case C_RED:     return Serial.write("\033[31m"); break;
-            case C_BLUE:    return Serial.write("\033[34m"); break;
-            case C_CYAN:    return Serial.write("\033[36m"); break;
-            case C_YELLOW:  return Serial.write("\033[33m"); break;
-            case C_MAGENTA: return Serial.write("\033[35m"); break;
-        }   return -1;
-    }
-
-    /*─······································································─*/
-
+    
     template< class V, class... T >
     int print( const V& argc, const T&... args ){ 
         Serial.write( (char*) string::format( (char*)argc, args... ) ); 
         return Serial.write("\033[0m"); 
     }
     
-    int gotoxy( int x, int y ){ return print("\033[%d;%dH",x,y); }
+    namespace { template< class V, class... T > 
+    int pout( const V& argc, const T&... args ){ 
+        return Serial.write( (char*) string::format( (char*)argc, args... ) ); 
+    }}
 
-    int undescore(){ return printf("\033[4m"); }
-
-    int inverse(){ return printf("\033[7m"); }
-
-    int reset(){ return printf("\033[0m"); }
-    
-    int clear(){ return printf("\033c"); }
-    
     /*─······································································─*/
 
-    int error( const char* msg ){ foreground( C_RED + C_BOLD ); return print( "%s", msg ); }
+    int gotoxy( int x, int y ){ return pout("\033[%d;%dH",x,y); }
 
-    int info( const char* msg ){ foreground( C_CYAN + C_BOLD ); return print( "%s", msg ); }
+    int undescore(){ return pout("\033[4m"); }
 
-    int done( const char* msg ){ foreground( C_GREEN + C_BOLD ); return print( "%s", msg ); }
+    int inverse(){ return pout("\033[7m"); }
 
-    int warn( const char* msg ){ foreground( C_YELLOW + C_BOLD ); return print( "%s", msg ); }
+    int reset(){ return pout("\033[0m"); }
+    
+    int clear(){ return pout("\033c"); }
+
+    /*─······································································─*/
+
+    int background( int color ){ 
+        if( color & 0x10 ){ pout("\033[1m"); color &= 0x0f; }
+        switch( color ) {
+            case C_BLACK:   return pout("\033[40m"); break;
+            case C_WHITE:   return pout("\033[47m"); break;
+            case C_GREEN:   return pout("\033[42m"); break;
+            case C_RED:     return pout("\033[41m"); break;
+            case C_BLUE:    return pout("\033[44m"); break;
+            case C_CYAN:    return pout("\033[46m"); break;
+            case C_YELLOW:  return pout("\033[43m"); break;
+            case C_MAGENTA: return pout("\033[45m"); break;
+        }   return -1;
+    }
+
+    int foreground( int color ){
+        if( color & 0x10 ){ pout("\033[1m"); color &= 0x0f; }
+        switch( color ) {
+            case C_BLACK:   return pout("\033[30m"); break;
+            case C_WHITE:   return pout("\033[37m"); break;
+            case C_GREEN:   return pout("\033[32m"); break;
+            case C_RED:     return pout("\033[31m"); break;
+            case C_BLUE:    return pout("\033[34m"); break;
+            case C_CYAN:    return pout("\033[36m"); break;
+            case C_YELLOW:  return pout("\033[33m"); break;
+            case C_MAGENTA: return pout("\033[35m"); break;
+        }   return -1;
+    }
+
+    /*─······································································─*/
+
+    int error( const char* msg ){ foreground( C_RED    | C_BOLD ); return print( "%s", msg ); }
+    int  info( const char* msg ){ foreground( C_CYAN   | C_BOLD ); return print( "%s", msg ); }
+    int  done( const char* msg ){ foreground( C_GREEN  | C_BOLD ); return print( "%s", msg ); }
+    int  warn( const char* msg ){ foreground( C_YELLOW | C_BOLD ); return print( "%s", msg ); }
 
 }}
 
