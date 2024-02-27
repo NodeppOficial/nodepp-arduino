3934caf42ee4ba09e16c53a950b64c804ffffa8f
edbcrepo <becerracenmanueld@gmail.com>
2024-02-27 08:30:28 -0400

    update

--- src/nodepp/generator.h
+++ src/nodepp/generator.h
@@ -1,28 +1,28 @@
 #define NODEPP_GENERATOR
 
-#if !defined(GENERATOR_TIMER) && defined(NODEPP_TIMER)
+#if !defined(GENERATOR_TIMER) && defined(NODEPP_TIMER) && defined(NODEPP_GENERATOR)
     #define  GENERATOR_TIMER
 namespace nodepp { namespace _timer_ {
 
     GENERATOR( timer ){ public:
 
         template< class V, class... T > 
-        gnEmit( V func, const ptr_t<ulong>& out, ulong time, const T&... args ){
+        gnEmit( V func, const ptr_t<int>& out, ulong time, ptr_t<ulong> stm, const T&... args ){
         gnStart
             if(*out == 0 )                   coEnd;
-            if( process::millis() <= *out )  coGoto(0);
-            if ( func(args...)<0 )           coEnd;
-            *out = process::millis() + time; coGoto(0); 
+            if( process::millis() <= *stm )  coGoto(0);
+            if( func(args...)<0 )            coEnd;
+            *stm = process::millis() + time; coGoto(0); 
         gnStop
         }
 
         template< class V, class... T > 
-        gnEmit( V func, const ptr_t<ulong>& out, ulong* time, const T&... args ){
+        gnEmit( V func, const ptr_t<int>& out, ulong* time, ptr_t<ulong> stm, const T&... args ){
         gnStart
-            do { if(*out == 0 ) {  coEnd; }   coNext;
-               } while( process::millis() <= *out ); 
-            if ( func(args...)<0 ){ coEnd; }
-            *out = process::millis() + *time; coGoto(0); 
+            if(*out == 0 )                   coEnd;
+            if( process::millis() <= *stm )  coGoto(0);
+            if( func(args...)<0 )            coEnd;
+            *stm = process::millis() +*time; coGoto(0); 
         gnStop
         }
 
@@ -33,28 +33,28 @@ namespace nodepp { namespace _timer_ {
     GENERATOR( utimer ){ public:
 
         template< class V, class... T > 
-        gnEmit( V func, const ptr_t<ulong>& out, ulong time, const T&... args ){
+        gnEmit( V func, const ptr_t<int>& out, ulong time, ptr_t<ulong> stm, const T&... args ){
         gnStart
-            do { if(*out == 0 ) {  coEnd; }   coNext;
-               } while( process::micros() <= *out ); 
-            if ( func(args...)<0 ){ coEnd; }
-            *out = process::micros() + time;  coGoto(0); 
+            if(*out == 0 )                   coEnd;
+            if( process::micros() <= *stm )  coGoto(0);
+            if( func(args...)<0 )            coEnd;
+            *stm = process::micros() + time; coGoto(0);
         gnStop
         }
 
         template< class V, class... T > 
-        gnEmit( V func, const ptr_t<ulong>& out, ulong* time, const T&... args ){
+        gnEmit( V func, const ptr_t<int>& out, ulong* time, ptr_t<ulong> stm, const T&... args ){
         gnStart
-            do { if(*out == 0 ) {  coEnd; }   coNext;
-               } while( process::micros() <= *out ); 
-            if ( func(args...)<0 ){ coEnd; } 
-            *out = process::micros() + *time; coGoto(0);
+            if(*out == 0 )                   coEnd;
+            if( process::micros() <= *stm )  coGoto(0);
+            if( func(args...)<0 )            coEnd;
+            *stm = process::micros() +*time; coGoto(0);
         gnStop
         }
 
     };
 
-}}
+}}  
 #undef NODEPP_GENERATOR
 #endif
 
